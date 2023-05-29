48689325a837672810ce2fd15782316e53ba30b9
Martin Ashton <martin.ashton@vimaec.com>
2023-05-29 16:18:54 -0400

    (WIP) Implementing runnable unit tests.

--- src/cpp/format/test.cpp
+++ src/cpp/format/test.cpp
@@ -52,13 +52,13 @@ void testElement(const Vim::DocumentModel& model)
     assert(model.mElement);
 
     test("Element count", model.mElement->GetCount(), 4464);
-    test("Element 0 ID", model.mElement->GetId(0), -1);
-    test("Element 1 ID", model.mElement->GetId(1), 1222722);
-    test("Element 2 ID", model.mElement->GetId(2), 32440);
-    test("Element 3 ID", model.mElement->GetId(3), 118390);
+    test("Element 0 ID", model.mElement->GetId(0), -1ll);
+    test("Element 1 ID", model.mElement->GetId(1), 1222722ll);
+    test("Element 2 ID", model.mElement->GetId(2), 32440ll);
+    test("Element 3 ID", model.mElement->GetId(3), 118390ll);
 
     test("Element 30 Index", model.mElement->Get(30)->mIndex, 30);
-    test("Element 30 ID", model.mElement->Get(30)->mId, 374011);
+    test("Element 30 ID", model.mElement->Get(30)->mId, 374011ll);
     test("Element 30 Name", *model.mElement->Get(30)->mName, std::string("GWB on Mtl. Stud"));
     test("Element 30 UniqueID", *model.mElement->Get(30)->mUniqueId, std::string("3ae43fb5-6797-479b-ac14-3436f35a7178-0005b4fb"));
     test("Element 30 FamilyNome", *model.mElement->Get(30)->mFamilyName, std::string("Compound Ceiling"));
@@ -85,8 +85,8 @@ void testElementIds(const Vim::DocumentModel& model)
 
     auto ids = model.mElement->GetAllId();
 
-    std::vector<int> expectedIds = { -1, 1222722, 32440, 118390, 174750, 18438, 355500, 185913, 9946, 182664 };
-    std::vector<int> actualIds = *ids;
+    std::vector<long long> expectedIds = { -1ll, 1222722ll, 32440ll, 118390ll, 174750ll, 18438ll, 355500ll, 185913ll, 9946ll, 182664ll };
+    std::vector<long long> actualIds = *ids;
     actualIds.resize(10);
 
     test("Element IDs count", (int) ids->size(), 4464);
@@ -106,15 +106,37 @@ void testGetAll(const Vim::DocumentModel& model)
     std::cout << "Get-all test: OK" << std::endl;
 }
 
+constexpr char pathSeparator =
+#if defined(__ANDROID__) || defined(__APPLE__)
+    '/';
+#else
+    '\\';
+#endif
+
+std::string normalizePath(const std::string& fileName)
+{
+    std::string ret = fileName;
+
+#if defined(__ANDROID__) || defined(__APPLE__)
+    std::replace(ret.begin(), ret.end(), '\\', platformPathSeparator);
+#else
+    std::replace(ret.begin(), ret.end(), '/', pathSeparator);
+#endif
+
+    return ret;
+}
+
 int main(int num, char** args)
 {
-    auto path = std::string(args[0]);
-    path = path.erase(path.find_last_of('/')) + "/data/Wolford_Residence.r2023.vim";
+    auto thisExePath = normalizePath(std::string(args[0]));
+    const auto thisDir = thisExePath.erase(thisExePath.rfind("src"));
+    const auto testRelativeFilePath = normalizePath("data\\Wolford_Residence.r2023.vim");
+    const auto vimFilePath = thisDir + testRelativeFilePath;
 
-    std::cout << "Testing VIM file: " << path << std::endl;
+    std::cout << "Testing VIM file: " << thisExePath << std::endl;
 
     Vim::VimScene scene;
-    scene.ReadFile(path);
+    scene.ReadFile(vimFilePath);
 
     Vim::DocumentModel model(scene);
 
