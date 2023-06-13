538cf0578990636781280a0d494b71a725de2632
Vadim Vorotilin <vadim.vorotilin@gmail.com>
2023-06-13 15:49:23 +0300

    Vim.Geometry included into Vim.Format

---
+++ src/cs/vim/Vim.Format/QuadMesh.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Vim.G3d;
+
+namespace Vim.Geometry
+{
+    /// <summary>
+    /// This is a quadrilateral mesh. Note that it does not implement the IMesh interface,
+    /// but does implement IGeometryAttributes and inherits from a G3D.
+    /// </summary>
+    public class QuadMesh : G3D, IGeometryAttributes
+    {
+        public QuadMesh(IEnumerable<GeometryAttribute> attributes)
+            : base(attributes.Append(new[] { 4 }.ToObjectFaceSizeAttribute()))
+            => Debug.Assert(NumCornersPerFace == 4);
+
+        public IMesh ToTriMesh()
+            => this.TriangulateQuadMesh().ToIMesh();
+    }
+}
