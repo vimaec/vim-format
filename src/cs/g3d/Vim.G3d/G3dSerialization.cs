95a19993fb50f2a34f1c032cc3780177825ac492
simon <simon.roberge@vimaec.com>
2024-01-29 15:28:11 -0500

    removed unused method

--- src/cs/g3d/Vim.G3d/G3dSerialization.cs
+++ src/cs/g3d/Vim.G3d/G3dSerialization.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.IO;
-using Vim.BFastNS;
+﻿using Vim.BFastNS;
 using Vim.LinqArray;
-using System.Collections.Generic;
 
 namespace Vim.G3d
 {
     public static partial class G3DExtension
     {
-        public static void WriteAttribute(Stream stream, GeometryAttribute attribute, string name, long size)
-        {
-            var buffer = attribute.ToBuffer();
-            if (buffer.NumBytes() != size)
-                throw new Exception($"Internal error while writing attribute, expected number of bytes was {size} but instead was {buffer.NumBytes()}");
-            if (buffer.Name != name)
-                throw new Exception($"Internal error while writing attribute, expected name was {name} but instead was {buffer.Name}");
-            stream.Write(buffer);
-        }
-
         public static BFast ToBFast(this IGeometryAttributes self, G3dHeader? header = null)
         {
             var bfast = new BFast();
@@ -28,8 +15,5 @@ namespace Vim.G3d
             }
             return bfast;
         }
-
-
-   
     }
 }
