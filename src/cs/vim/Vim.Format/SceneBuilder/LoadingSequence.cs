9affe332523cd6e70696654d1482db6c795ab7bd
simon <simon.roberge@vimaec.com>
2024-04-17 10:43:30 -0400

    extracting loading code

---
+++ src/cs/vim/Vim.Format/SceneBuilder/LoadingSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Vim.Format.SceneBuilder
+{
+    public class LoadingSequence : ILoadingStep
+    {
+        private readonly LoadingStep[] _steps;
+        public float Effort { get; }
+
+        public LoadingSequence(params LoadingStep[] steps)
+        {
+            _steps = steps;
+            Effort = _steps.Sum(s => s.Effort);
+        }
+
+        public void Run(ILoadingProgress progress)
+        {
+            foreach (var step in _steps)
+                step.Run(progress);
+        }
+    }
+}
