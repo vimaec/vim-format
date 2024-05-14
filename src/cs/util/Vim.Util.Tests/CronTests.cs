e0ca95b197d2d0cf91dbda3491bf36b7572b8fca
Martin Ashton <martin.ashton@vimaec.com>
2024-05-14 12:58:48 -0400

    Added Cron expression implementation and tests

---
+++ src/cs/util/Vim.Util.Tests/CronTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+using static Vim.Util.Cron;
+
+namespace Vim.Util.Tests;
+
+[TestFixture]
+public static class CronTests
+{
+    public record GTC(Days[] Days, TimeSpan TimeOfDay, string Expected);
+
+    public static IEnumerable<GTC> GenerateTestCases() => new[]
+    {
+        new GTC(Array.Empty<Days>(), default, "0 0 0 ? * "),
+        new GTC(new[] { Days.SUN }, new TimeSpan(23, 59, 58), "58 59 23 ? * 0"),
+        new GTC(new [] { Days.MON, Days.WED, Days.FRI }, new TimeSpan(19, 0, 0), "0 0 19 ? * 1,3,5")
+    };
+
+    [TestCaseSource(nameof(GenerateTestCases))]
+    public static void TestGenerate(GTC c)
+    {
+        var s = GenerateCronExpression(c.Days, c.TimeOfDay);
+        Assert.AreEqual(c.Expected, s);
+    }
+
+    public record PTC(string CronExpression, bool ExpectedSuccess, Days[] ExpectedDays, TimeSpan ExpectedTimeOfDay);
+
+    public static IEnumerable<PTC> ParseTestCases() => new[]
+    {
+        new PTC(null, false, null, default),
+        new PTC("", false, null, default),
+        new PTC(" ", false, null, default),
+        new PTC("0-5", false, null, default),
+        new PTC("0 0 0 ? *", false, null, default),
+        new PTC("58 59 23 ? * 0",
+            true, 
+            new [] { Days.SUN },
+            new TimeSpan(23, 59, 58)),
+        new PTC("0 0 19 ? * 1,3,5",
+            true,
+            new [] { Days.MON, Days.WED, Days.FRI },
+            new TimeSpan(19, 0, 0)),
+        new PTC("0 0 7 ? * 4-6",
+            true,
+            new [] { Days.THU, Days.FRI, Days.SAT},
+            new TimeSpan(7, 0, 0)),
+        new PTC("0 0 7 ? * *",
+            true,
+            new [] { Days.SUN, Days.MON, Days.TUE, Days.WED, Days.THU, Days.FRI, Days.SAT},
+            new TimeSpan(7, 0, 0)),
+        new PTC("0 0 7 ? * 7", // Test that we wrap around using modulo 7 (7 % 7 = 0 = SUN)
+            true,
+            new [] { Days.SUN },
+            new TimeSpan(7, 0, 0)),
+        new PTC("0 0 7 ? * 14", // Test that we wrap around using modulo 7 (14 % 7 = 0 = SUN)
+            true,
+            new [] { Days.SUN },
+            new TimeSpan(7, 0, 0)),
+        new PTC("0 0 7 ? * 13", // Test that we wrap around using modulo 7 (13 % 7 = 6 = SAT)
+            true,
+            new [] { Days.SAT },
+            new TimeSpan(7, 0, 0)),
+    };
+
+    [TestCaseSource(nameof(ParseTestCases))]
+    public static void TestParse(PTC c)
+    {
+        var success = TryParseCronExpression(c.CronExpression, out var d, out var t);
+        Assert.AreEqual(c.ExpectedSuccess, success);
+        Assert.AreEqual(c.ExpectedDays, d);
+        Assert.AreEqual(c.ExpectedTimeOfDay, t);
+    }
+}
---
+++ src/cs/util/Vim.Util/Cron.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vim.Util
+{
+    public static class Cron
+    {
+        public enum Days
+        {
+            SUN = 0,
+            MON = 1,
+            TUE = 2,
+            WED = 3,
+            THU = 4,
+            FRI = 5,
+            SAT = 6,
+        }
+
+        public static IReadOnlyList<string> DayNames => Enum.GetNames(typeof(Days));
+
+        public static string GenerateCronExpression(IEnumerable<Days> cronDaysOfTheWeek, TimeSpan timeOfDay)
+            => $"{timeOfDay.Seconds} {timeOfDay.Minutes} {timeOfDay.Hours} ? * {string.Join(",", cronDaysOfTheWeek.Select(d => (int)d))}";
+
+        public static bool TryParseCronExpression(string cronExpression, out Days[] daysOfWeek, out TimeSpan timeOfDay)
+        {
+            daysOfWeek = null;
+            timeOfDay = default;
+
+            if (string.IsNullOrWhiteSpace(cronExpression))
+                return false;
+
+            try
+            {
+                // Split the cron expression into its components
+                var cronComponents = cronExpression.Split(' ');
+                if (cronComponents.Length < 6)
+                    return false;
+
+                // Extract the time of day components (hour and minute)
+                var second = int.Parse(cronComponents[0]);
+                var minute = int.Parse(cronComponents[1]);
+                var hour = int.Parse(cronComponents[2]);
+
+                // Convert hour and minute to TimeSpan representing the time of day
+                timeOfDay = new TimeSpan(hour, minute, second);
+
+                // Extract the day of week component (0-indexed)
+                var dayOfWeekComponent = cronComponents[5];
+
+                // Convert the day of week component to a list of integers representing selected days
+                daysOfWeek = ParseDayOfWeekComponent(dayOfWeekComponent);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static Days[] ParseDayOfWeekComponent(string dayOfWeekComponent)
+        {
+            var daysOfWeek = new List<Days>();
+
+            var allDaysAsInts = Enum.GetValues(typeof(Days)).Cast<int>().ToArray();
+            var minDay = allDaysAsInts.Min();
+            var maxDay = allDaysAsInts.Max();
+
+            void AddDay(int day)
+            {
+                if (day < minDay)
+                    return;
+
+                day %= (maxDay + 1); // 7 (also SUN) becomes 0 (SUN)
+
+                daysOfWeek.Add((Days) day);
+            }
+
+            if (dayOfWeekComponent.Contains(","))
+            {
+                // Handle comma-separated list of days
+                var parts = dayOfWeekComponent.Split(',');
+                foreach (var part in parts)
+                {
+                    if (int.TryParse(part, out var day))
+                    {
+                        AddDay(day);
+                    }
+                }
+            }
+            else if (dayOfWeekComponent.Contains("-"))
+            {
+                // Handle range of days
+                var range = dayOfWeekComponent.Split('-');
+                if (range.Length == 2 &&
+                    int.TryParse(range[0], out var start) &&
+                    int.TryParse(range[1], out var end))
+                {
+                    for (var i = start; i <= end; i++)
+                    {
+                        AddDay(i);
+                    }
+                }
+            }
+            else if (dayOfWeekComponent.Equals("*"))
+            {
+                // Handle all days of the week
+                daysOfWeek.AddRange(Enum.GetValues(typeof(Days)).Cast<Days>());
+            }
+            else if (int.TryParse(dayOfWeekComponent, out var day))
+            {
+                // Handle single day
+                AddDay(day);
+            }
+
+            return daysOfWeek.ToArray();
+        }
+    }
+}
