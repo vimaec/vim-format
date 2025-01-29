4bec1cbed7f64e240f7bac8a3153e74f6cea8721
Martin Ashton <martin.ashton@vimaec.com>
2025-01-29 10:56:38 -0500

    Added convenience function to create a box mesh

--- src/cs/vim/Vim.Format.Core/DocumentBuilderTypes.cs
+++ src/cs/vim/Vim.Format.Core/DocumentBuilderTypes.cs
@@ -164,6 +164,46 @@ namespace Vim.Format
                    && Indices.SequenceEqual(other.Indices)
                     && SubmeshesIndexOffset.SequenceEqual(other.SubmeshesIndexOffset)
                     && SubmeshMaterials.SequenceEqual(other.SubmeshMaterials);
+
+            public static SubdividedMesh CreateFromWorldSpaceBox(IReadOnlyList<Vector3> points, int materialIndex)
+            {
+                //     7 *--------------------------* 6 (max)
+                //       |\                         |\
+                //       | \                        | \
+                //       |  \ 4                     |  \ 5
+                //       |   *--------------------------*
+                //       |   |                      |   |
+                //     3 *---|----------------------* 2 |
+                //        \  |                       \  |
+                //         \ |                        \ |
+                //          \|                         \|
+                //           *--------------------------*
+                //    (min) 0                             1
+
+                // Faces are declared as quads in counter-clockwise order (right-handed rule, thumb points in normal direction)
+                var bottomQuad = new[] { points[0], points[3], points[2], points[1] };
+                var topQuad = new[] { points[4], points[5], points[6], points[7] };
+                var backQuad = new[] { points[2], points[3], points[7], points[6] };
+                var frontQuad = new[] { points[0], points[1], points[5], points[4] };
+                var leftQuad = new[] { points[3], points[0], points[4], points[7] };
+                var rightQuad = new[] { points[2], points[6], points[5], points[1] };
+
+                var quads = new[] { bottomQuad, topQuad, backQuad, frontQuad, leftQuad, rightQuad };
+
+                var vertices = new List<Vector3>();
+                var indices = new List<int>();
+
+                var quadIndices = new[] { 0, 1, 2, 0, 2, 3 };
+
+                foreach (var quadVertices in quads)
+                {
+                    var indexOffset = vertices.Count;
+                    vertices.AddRange(quadVertices);
+                    indices.AddRange(quadIndices.Select(i => i + indexOffset));
+                }
+
+                return new SubdividedMesh(indices, vertices, new[] { 0 }, new[] { materialIndex });
+            }
         }
     }
 }
