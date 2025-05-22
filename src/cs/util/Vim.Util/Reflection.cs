f744408fe939f88725b0eba9f82573d4d57e4dce
Martin Ashton <martin.ashton@vimaec.com>
2025-05-21 23:27:40 -0400

    Implemented Vim.Format.ILMerge and its product Vim.Format.Standalone.dll

--- src/cs/util/Vim.Util/Reflection.cs
+++ src/cs/util/Vim.Util/Reflection.cs
@@ -19,17 +19,10 @@ namespace Vim.Util
             => self.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
         /// <summary>
-        /// Ignores the property or the field when they are being enumerated from a given object.
-        /// </summary>
-        [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-        public class IgnoreInReflectedListAttribute : Attribute { }
-
-        /// <summary>
         /// Converts fields to a dictionary of string/object pair.
         /// </summary>
         public static IDictionary<string, object> FieldsToDictionary(this object self)
             => self.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance)
-                .Where(fi => !fi.GetCustomAttributes<IgnoreInReflectedListAttribute>().Any())
                 .ToDictionary(fi => fi.Name, fi => fi.GetValue(self));
 
         /// <summary>
@@ -37,13 +30,16 @@ namespace Vim.Util
         /// </summary>
         public static IDictionary<string, object> PropertiesToDictionary(this object self)
             => self.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(pi => !pi.GetCustomAttributes<IgnoreInReflectedListAttribute>().Any())
                 .ToDictionary(pi => pi.Name, fi => fi.GetValue(self));
 
         /// <summary>
         /// Converts properties to an enumerable of strings.
         /// </summary>
-        public static IEnumerable<string> PropertiesToStrings(this object self)
-            => self.PropertiesToDictionary().Select(kv => $"{kv.Key}: {kv.Value}");
+        public static IEnumerable<string> PropertiesToStrings(this object self, Func<string, bool> keyFilter = null)
+        {
+            return self.PropertiesToDictionary()
+                .Where(kv => keyFilter?.Invoke(kv.Key) ?? true)
+                .Select(kv => $"{kv.Key}: {kv.Value}");
+        }
     }
 }
---
+++ src/cs/vim/Vim.Format.ILMerge.Tests/VimFormatILMergeTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.IO;
+using Vim.Util.Logging;
+
+namespace Vim.Format.ILMerge.Tests;
+
+[TestFixture]
+public static class VimFormatILMergeTests
+{
+    [Test]
+    public static void TestVimFormatStandalone()
+    {
+        // This test makes use of the merged version of serilog.
+        // We had to create a merged standalone version of Vim.Format to minimize collisions with other assemblies in Revit.
+        var fileName = "standalone.log";
+
+        var logger = Util.Logging.Serilog.Log.Init("toot", fileName, true);
+        logger.LogInformation("this is a log message for the baked-in version of serilog");
+
+        var fileInfo = new FileInfo(fileName);
+        Assert.IsTrue(fileInfo.Exists, $"File not found: {fileInfo.FullName}");
+    }
+}
