2a0e183f269c6a390cfc74016f6dfd96a284c6d2
Martin Ashton <martin.ashton@vimaec.com>
2025-10-23 11:29:56 -0400

    Refactored column name component parsing. Added TryGetColumnByFieldName

--- src/cs/vim/Vim.Format.Core/ColumnExtensions.cs
+++ src/cs/vim/Vim.Format.Core/ColumnExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Vim.BFast;
@@ -62,52 +63,75 @@ namespace Vim.Format
         public static string GetIndexColumnName(string relatedTableName, string localFieldName)
             => VimConstants.IndexColumnNameTypePrefix + relatedTableName + RelatedTableNameFieldNameSeparator + localFieldName;
 
-        public class StringOrDataColumnNameComponents
+        public class ColumnNameComponents
         {
-            public static readonly Regex StringOrDataColumnNameComponentsRegex = new Regex(@"(\w+:)(.+)");
+            public static readonly Regex ColumnNameComponentsRegex = new Regex(@"(\w+:)(.+)", RegexOptions.Compiled);
+            public static readonly Regex IndexColumnNameComponentsRegex = new Regex(@"(\w+:)((?:\w|\.)+):(.+)", RegexOptions.Compiled);
 
             public readonly string TypePrefix;
             public readonly string FieldName;
+            public readonly string RelatedTableName; // only applicable for index columns.
+            public readonly ColumnType ColumnType;
 
-            public StringOrDataColumnNameComponents(string stringOrDataColumnName)
+            public ColumnNameComponents(string columnName)
             {
-                var match = StringOrDataColumnNameComponentsRegex.Match(stringOrDataColumnName);
+                var match = ColumnNameComponentsRegex.Match(columnName);
                 if (!match.Success)
-                    throw new Exception($"Column name {stringOrDataColumnName} could not be separated into its components.");
+                    throw new Exception($"Column name {columnName} could not be separated into its components.");
 
+                // Collect the type prefix.
                 TypePrefix = match.Groups[1].Value;
+                switch (TypePrefix)
+                {
+                    case VimConstants.IndexColumnNameTypePrefix:
+                        ColumnType = ColumnType.IndexColumn;
+                        break;
+                    case VimConstants.StringColumnNameTypePrefix:
+                        ColumnType = ColumnType.StringColumn;
+                        break;
+                    default:
+                        ColumnType = ColumnType.DataColumn;
+                        break;
+                }
+
+                // Collect the field name
                 FieldName = match.Groups[2].Value;
+
+                // If it's an index column, get the related table.
+                if (ColumnType == ColumnType.IndexColumn)
+                {
+                    var indexMatch = IndexColumnNameComponentsRegex.Match(columnName);
+                    if (!match.Success)
+                        throw new Exception($"Index column name {columnName} could not be separated into its components.");
+
+                    RelatedTableName = indexMatch.Groups[2].Value;
+                }
             }
-        }
 
-        public class IndexColumnNameComponents
-        {
-            public static readonly Regex IndexColumnNameComponentsRegex = new Regex(@"(\w+:)((?:\w|\.)+):(.+)");
-
-            public readonly string TypePrefix;
-            public readonly string TableName;
-            public readonly string FieldName;
-
-            public IndexColumnNameComponents(string indexColumnName)
+            public static bool TryGetColumnNameComponents(string columnName, out ColumnNameComponents columnComponents)
             {
-                var match = IndexColumnNameComponentsRegex.Match(indexColumnName);
-                if (!match.Success)
-                    throw new Exception($"Index column name {indexColumnName} could not be separated into its components.");
+                columnComponents = null;
 
-                TypePrefix = match.Groups[1].Value;
-                TableName = match.Groups[2].Value;
-                FieldName = match.Groups[3].Value;
+                try
+                {
+                    columnComponents = new ColumnNameComponents(columnName);
+                }
+                catch (Exception e)
+                {
+                    // Invalid column name; could not be parsed.
+                    Debug.Fail($"Column components could not be parsed: {columnName}. Error: {e}");
+                    return false;
+                }
+
+                return true;
             }
         }
-
-        public static IndexColumnNameComponents SplitIndexColumnName(string name)
-            => new IndexColumnNameComponents(name);
 
         public static string GetRelatedTableNameFromColumnName(string name)
-            => SplitIndexColumnName(name).TableName;
+            => new ColumnNameComponents(name).RelatedTableName;
 
         public static string GetFieldNameFromIndexColumnName(string name)
-            => SplitIndexColumnName(name).FieldName;
+            => new ColumnNameComponents(name).FieldName;
 
         public static string GetRelatedTableName(this INamedBuffer<int> ic)
             => GetRelatedTableNameFromColumnName(ic.Name);
@@ -119,16 +143,45 @@ namespace Vim.Format
             => GetFieldNameFromIndexColumnName(ic.Name);
 
         public static string GetStringColumnFieldName(this INamedBuffer<int> sc)
-            => new StringOrDataColumnNameComponents(sc.Name).FieldName;
+            => new ColumnNameComponents(sc.Name).FieldName;
 
         public static string GetDataColumnFieldName(this INamedBuffer dc)
-            => new StringOrDataColumnNameComponents(dc.Name).FieldName;
+            => new ColumnNameComponents(dc.Name).FieldName;
 
         public static bool TryGetDataColumnType(this INamedBuffer dc, out Type type)
         {
             type = null;
-            var typePrefix = new StringOrDataColumnNameComponents(dc.Name).TypePrefix;
+            var typePrefix = new ColumnNameComponents(dc.Name).TypePrefix;
             return TypePrefixToSerializedTypeMap.TryGetValue(typePrefix, out type);
+        }
+
+        public static bool TryGetColumnByFieldName(
+            this SerializableEntityTable entityTable,
+            string columnFieldName,
+            out INamedBuffer buffer,
+            out string typePrefix,
+            out ColumnType columnType)
+        {
+            buffer = null;
+            columnType = ColumnType.IndexColumn;
+            typePrefix = null;
+
+            foreach (var column in entityTable.GetAllColumns())
+            {
+                if (!ColumnNameComponents.TryGetColumnNameComponents(column.Name, out var components))
+                    continue;
+
+                if (components.FieldName != columnFieldName)
+                    continue;
+
+                columnType = components.ColumnType;
+                typePrefix = components.TypePrefix;
+                buffer = column;
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
