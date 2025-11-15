f0c95e68617b2d095ddca56eea4a75dcfba54254
martin <martin.ashton@gmail.com>
2025-11-14 22:13:51 -0500

    Moved ReferenceAppTests.cs to api_v2

--- src/cs/vim/Vim.Format.Tests/api_v2/ReferenceAppTests.cs
+++ src/cs/vim/Vim.Format.Tests/api_v2/ReferenceAppTests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using NUnit.Framework;
 using Vim.Util.Tests;
 
-namespace Vim.Format.Tests;
+namespace Vim.Format.Tests.api_v2;
 
 [TestFixture]
 public static class ReferenceAppTests
