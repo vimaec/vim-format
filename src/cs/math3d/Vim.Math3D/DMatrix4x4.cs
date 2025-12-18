593f3de645d8f9799f933396afd62f3c36a41c10
Martin Ashton <martin.ashton@vimaec.com>
2025-12-18 14:12:39 -0500

    Added DMatrix4x4 invert and some extension functions

--- src/cs/math3d/Vim.Math3D/DMatrix4x4.cs
+++ src/cs/math3d/Vim.Math3D/DMatrix4x4.cs
@@ -638,5 +638,173 @@ namespace Vim.Math3d
                 (float) M31, (float) M32, (float) M33, (float) M34,
                 (float) M41, (float) M42, (float) M43, (float) M44);
         }
+
+        /// <summary>
+        /// Attempts to calculate the inverse of the given matrix. If successful, result will contain the inverted matrix.
+        /// </summary>
+        /// <param name="matrix">The source matrix to invert.</param>
+        /// <param name="result">If successful, contains the inverted matrix.</param>
+        /// <returns>True if the source matrix could be inverted; False otherwise.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool Invert(DMatrix4x4 matrix, out DMatrix4x4 result)
+        {
+            //                                       -1
+            // If you have matrix M, inverse Matrix M   can compute
+            //
+            //     -1       1      
+            //    M   = --------- A
+            //            det(M)
+            //
+            // A is adjugate (adjoint) of M, where,
+            //
+            //      T
+            // A = C
+            //
+            // C is Cofactor matrix of M, where,
+            //           i + j
+            // C   = (-1)      * det(M  )
+            //  ij                    ij
+            //
+            //     [ a b c d ]
+            // M = [ e f g h ]
+            //     [ i j k l ]
+            //     [ m n o p ]
+            //
+            // First Row
+            //           2 | f g h |
+            // C   = (-1)  | j k l | = + ( f ( kp - lo ) - g ( jp - ln ) + h ( jo - kn ) )
+            //  11         | n o p |
+            //
+            //           3 | e g h |
+            // C   = (-1)  | i k l | = - ( e ( kp - lo ) - g ( ip - lm ) + h ( io - km ) )
+            //  12         | m o p |
+            //
+            //           4 | e f h |
+            // C   = (-1)  | i j l | = + ( e ( jp - ln ) - f ( ip - lm ) + h ( in - jm ) )
+            //  13         | m n p |
+            //
+            //           5 | e f g |
+            // C   = (-1)  | i j k | = - ( e ( jo - kn ) - f ( io - km ) + g ( in - jm ) )
+            //  14         | m n o |
+            //
+            // Second Row
+            //           3 | b c d |
+            // C   = (-1)  | j k l | = - ( b ( kp - lo ) - c ( jp - ln ) + d ( jo - kn ) )
+            //  21         | n o p |
+            //
+            //           4 | a c d |
+            // C   = (-1)  | i k l | = + ( a ( kp - lo ) - c ( ip - lm ) + d ( io - km ) )
+            //  22         | m o p |
+            //
+            //           5 | a b d |
+            // C   = (-1)  | i j l | = - ( a ( jp - ln ) - b ( ip - lm ) + d ( in - jm ) )
+            //  23         | m n p |
+            //
+            //           6 | a b c |
+            // C   = (-1)  | i j k | = + ( a ( jo - kn ) - b ( io - km ) + c ( in - jm ) )
+            //  24         | m n o |
+            //
+            // Third Row
+            //           4 | b c d |
+            // C   = (-1)  | f g h | = + ( b ( gp - ho ) - c ( fp - hn ) + d ( fo - gn ) )
+            //  31         | n o p |
+            //
+            //           5 | a c d |
+            // C   = (-1)  | e g h | = - ( a ( gp - ho ) - c ( ep - hm ) + d ( eo - gm ) )
+            //  32         | m o p |
+            //
+            //           6 | a b d |
+            // C   = (-1)  | e f h | = + ( a ( fp - hn ) - b ( ep - hm ) + d ( en - fm ) )
+            //  33         | m n p |
+            //
+            //           7 | a b c |
+            // C   = (-1)  | e f g | = - ( a ( fo - gn ) - b ( eo - gm ) + c ( en - fm ) )
+            //  34         | m n o |
+            //
+            // Fourth Row
+            //           5 | b c d |
+            // C   = (-1)  | f g h | = - ( b ( gl - hk ) - c ( fl - hj ) + d ( fk - gj ) )
+            //  41         | j k l |
+            //
+            //           6 | a c d |
+            // C   = (-1)  | e g h | = + ( a ( gl - hk ) - c ( el - hi ) + d ( ek - gi ) )
+            //  42         | i k l |
+            //
+            //           7 | a b d |
+            // C   = (-1)  | e f h | = - ( a ( fl - hj ) - b ( el - hi ) + d ( ej - fi ) )
+            //  43         | i j l |
+            //
+            //           8 | a b c |
+            // C   = (-1)  | e f g | = + ( a ( fk - gj ) - b ( ek - gi ) + c ( ej - fi ) )
+            //  44         | i j k |
+            //
+            // Cost of operation
+            // 53 adds, 104 muls, and 1 div.
+            double a = matrix.M11, b = matrix.M12, c = matrix.M13, d = matrix.M14;
+            double e = matrix.M21, f = matrix.M22, g = matrix.M23, h = matrix.M24;
+            double i = matrix.M31, j = matrix.M32, k = matrix.M33, l = matrix.M34;
+            double m = matrix.M41, n = matrix.M42, o = matrix.M43, p = matrix.M44;
+
+            var kp_lo = k * p - l * o;
+            var jp_ln = j * p - l * n;
+            var jo_kn = j * o - k * n;
+            var ip_lm = i * p - l * m;
+            var io_km = i * o - k * m;
+            var in_jm = i * n - j * m;
+
+            var a11 = +(f * kp_lo - g * jp_ln + h * jo_kn);
+            var a12 = -(e * kp_lo - g * ip_lm + h * io_km);
+            var a13 = +(e * jp_ln - f * ip_lm + h * in_jm);
+            var a14 = -(e * jo_kn - f * io_km + g * in_jm);
+
+            var det = a * a11 + b * a12 + c * a13 + d * a14;
+
+            if (det.Abs() < float.Epsilon)
+            {
+                result = new DMatrix4x4(float.NaN, float.NaN, float.NaN, float.NaN,
+                                       float.NaN, float.NaN, float.NaN, float.NaN,
+                                       float.NaN, float.NaN, float.NaN, float.NaN,
+                                       float.NaN, float.NaN, float.NaN, float.NaN);
+                return false;
+            }
+
+            var invDet = 1.0f / det;
+
+            result.M11 = a11 * invDet;
+            result.M21 = a12 * invDet;
+            result.M31 = a13 * invDet;
+            result.M41 = a14 * invDet;
+
+            result.M12 = -(b * kp_lo - c * jp_ln + d * jo_kn) * invDet;
+            result.M22 = +(a * kp_lo - c * ip_lm + d * io_km) * invDet;
+            result.M32 = -(a * jp_ln - b * ip_lm + d * in_jm) * invDet;
+            result.M42 = +(a * jo_kn - b * io_km + c * in_jm) * invDet;
+
+            var gp_ho = g * p - h * o;
+            var fp_hn = f * p - h * n;
+            var fo_gn = f * o - g * n;
+            var ep_hm = e * p - h * m;
+            var eo_gm = e * o - g * m;
+            var en_fm = e * n - f * m;
+
+            result.M13 = +(b * gp_ho - c * fp_hn + d * fo_gn) * invDet;
+            result.M23 = -(a * gp_ho - c * ep_hm + d * eo_gm) * invDet;
+            result.M33 = +(a * fp_hn - b * ep_hm + d * en_fm) * invDet;
+            result.M43 = -(a * fo_gn - b * eo_gm + c * en_fm) * invDet;
+
+            var gl_hk = g * l - h * k;
+            var fl_hj = f * l - h * j;
+            var fk_gj = f * k - g * j;
+            var el_hi = e * l - h * i;
+            var ek_gi = e * k - g * i;
+            var ej_fi = e * j - f * i;
+
+            result.M14 = -(b * gl_hk - c * fl_hj + d * fk_gj) * invDet;
+            result.M24 = +(a * gl_hk - c * el_hi + d * ek_gi) * invDet;
+            result.M34 = -(a * fl_hj - b * el_hi + d * ej_fi) * invDet;
+            result.M44 = +(a * fk_gj - b * ek_gi + c * ej_fi) * invDet;
+
+            return true;
+        }
     }
 }
